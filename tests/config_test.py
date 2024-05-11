1edeeedf994e7fb5bb2562ddfade0998f7d38be6
Kyle Holzinger <kyleholzinger@hey.com>
2024-05-11 16:03:56 -0400

    [KH] add more tests

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,6 +1,7 @@
 import os
+import pytest
 
-from config_profile.config import Config
+from config_profile import Config, RequiredConfigKeyException
 
 def test_base_config():
     config = Config(resource_dir=os.path.abspath(os.path.join(os.path.dirname(__file__), "fixtures", "resources")))
@@ -12,3 +13,15 @@ def test_config_profile_ordering():
     config = Config(resource_dir=os.path.abspath(os.path.join(os.path.dirname(__file__), "fixtures", "resources")))
 
     assert config.get("top_level_key.key_to_be_overwritten") == "I have been overwritten"
+
+def test_get():
+    config = Config(resource_dir=os.path.abspath(os.path.join(os.path.dirname(__file__), "fixtures", "resources")))
+
+    assert config.get("top_level_key.nonexistant_key", "default_value") == "default_value"
+
+def test_get_required():
+    config = Config(resource_dir=os.path.abspath(os.path.join(os.path.dirname(__file__), "fixtures", "resources")))
+
+    assert config.get_required("top_level_key.some_sub_key") == "a value"
+    with pytest.raises(RequiredConfigKeyException):
+        config.get_required("top_level_key.nonexistant_key")
