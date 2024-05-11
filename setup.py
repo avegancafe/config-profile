d4853295f432e343c790d8049069a176150cb0c4
Kyle Holzinger <kyleholzinger@hey.com>
2024-05-11 16:20:34 -0400

    [KH] add classifiers to pip

--- setup.py
+++ setup.py
@@ -5,4 +5,10 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
